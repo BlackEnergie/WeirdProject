1e443d440599373b9b26db7857ee00bd4350c364
Nicolas <nicolas33160@gmail.com>
2018-12-07 05:43:38 +0100

    bouton magique danger et fond d'écran

--- index.php
+++ index.php
@@ -14,16 +14,24 @@ session_start() ?>
         @import url(styles/weird.css);
     </style>
     <script src="script/weird.js"></script>
+    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 <body>
+
 <div>
     <?php
 
     include_once "views/constant/header.php";
 
     ?>
+    <div id="surprise" style="display:none;">
+        <img src="images/boi.jpg">
+    </div>
 </div>
+
+
+<button type="button" onclick="surprise()" class="btn btn-danger">Click if you are in danger</button>
 <script src="script/popper.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
         integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
@@ -31,5 +39,11 @@ session_start() ?>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
         crossorigin="anonymous"></script>
+        <script src="script/konami.js"></script>
+
+
+
+
+
 </body>
 </html>
